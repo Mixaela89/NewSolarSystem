31ed2c646063875665b5216772df11ef29175a42
Mixaela89 <mihela52119@gmail.com>
2022-12-18 12:41:19 -0500

    sun added but has errors that need to be resolved

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 SolarSystem ss =new SolarSystem();
 ss.planet=9;
+
+featureSun fs=new featureSun();
+fs.heat=12345;
+fs.size="1234";
     }
 }
